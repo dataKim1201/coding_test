bed5527d9ac5cbe3472287abdb2e27604fadc768
datakim1201 <gkstjd1201@soongsil.ac.kr>
2024-03-04 23:02:29 +0900

    2024/03/04

---
+++ BOJ/15829.py
@@ -0,0 +1,7 @@
+# r의 값은 26보다 큰 소수인 31로 하고 M의 값은 1234567891
+import sys
+input = sys.stdin.readline
+n  = int(input())
+tt = input()
+res = sum([(ord(tt[i])-96) * (31**i) for i in range(n)])
+print(res)
