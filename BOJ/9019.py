834822825b89f4b80624bf0d5adf913d52462f53
datakim1201 <gkstjd1201@soongsil.ac.kr>
2024-04-07 17:01:23 +0900

    2024/04/07

---
+++ BOJ/9019.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def change(case,cmd):
+    if cmd == 'D': return case * 2 % 10000
+    elif cmd == 'S': return case -1 % 10000
+    elif cmd == 'L': return case*10 % 1000 + case // 1000
+    elif cmd == "D": return case % 10 * 1000 + case//10
+def bfs(case, target):
+    queue = deque([(case, "")])
+    command = ["D", "S", "L", "R"]
+    visited = [False]*10000
+    visited[case] = True
+    while queue:
+        tmp, cmd = queue.popleft()
+        if tmp == target:
+            return cmd
+        for i in command:
+            new_tmp = change(tmp,i)
+            if not visited[new_tmp]:
+                queue.append([new_tmp,cmd + i])
+                visited[new_tmp] = True
+
+
+T = int(input().strip())
+res = []
+for _ in range(T):
+    case, target = map(int,input().split())
+    res.append(bfs(case,target))
+print('\n'.join(res))
