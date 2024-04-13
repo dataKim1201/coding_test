82581b5caedd03cd06bfe2f4fd05dc74202bbf9a
datakim1201 <gkstjd1201@soongsil.ac.kr>
2024-04-13 17:53:50 +0900

    2024/04/13

---
+++ BOJ/11403.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def check(x,y):
+    visited = []
+    queue = deque([x])
+    while queue:
+        x = queue.popleft()
+        if x == y and len(visited) > 0:
+            return 1
+        for item in G[x]:
+            if item not in visited:
+                visited.append(item)
+                queue.append(item)
+    return 0
+
+if __name__ == '__main__':
+    N = int(input())
+    zido = [list(map(int,input().split())) for _ in range(N)]
+    G = {}
+    for i in range(N):
+        G[i] = []
+        for j in range(N):
+            if zido[i][j] == 1:
+                G[i].append(j)
+    # print("result", check(0,2))
+    for i in range(N):
+        for j in range(N):
+            print(check(i,j),end=' ')
+        print()
