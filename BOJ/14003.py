07ca09eea563fe5055fdeb651bee935f4b64fc0e
datakim1201 <gkstjd1201@soongsil.ac.kr>
2025-03-18 22:57:35 +0900

    250318

--- BOJ/14003.py
+++ BOJ/14003.py
@@ -1,30 +1,29 @@
-import sys
 from bisect import bisect_left
-input = sys.stdin.readline
+from copy import deepcopy
 
-def is_sub(arr):
-    dist = [0 for _ in range(len(arr))]
-    lst = [arr[0]]
-    for i in range(len(arr)):
-        if lst[-1] < arr[i]:
-            lst.append(arr[i])
-            dist[i] = len(lst) - 1
-        else:
-            idx = bisect_left(lst,arr[i]) # 어디에 넣을지
-            lst[idx] = arr[i]
-            dist[i] = i
-    # 출력하기
-    final_cnt = len(lst)
-    print(final_cnt)
-    res = []
-    for i in range(len(dist) -1, -1, -1):
-        if dist[i] == final_cnt:
-            res.append(arr[i])
-            final_cnt -= 1
-    print(*res[::-1])
-            
-if __name__ == '__main__':
-    n = int(input())
-    arr = list(map(int,input().split()))
-    is_sub(arr)
-    
+n = int(input())
+a = list(map(int, input().split()))
+
+lst = [a[0]]
+memoization = [0 for _ in range(n)]
+
+for idx, num in enumerate(a[1:], start = 1) :
+    if lst[-1] < num :
+        lst.append(num)
+        memoization[idx] = len(lst)-1
+    else :
+        b_idx = bisect_left(lst, num)
+        lst[b_idx] = num
+        memoization[idx] = b_idx
+
+
+maxVal = len(lst)
+print(maxVal)
+answer = []
+maxVal -= 1
+for idx in range(n-1, -1, -1) :
+    if memoization[idx] == maxVal :
+        answer.append(a[idx])
+        maxVal -= 1
+answer.reverse()
+print(* answer)
